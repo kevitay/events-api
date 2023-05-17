0a625754bc6ebe395710b3aa5e9093a4511024d4
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-17 14:52:04 -0400

    Red failing get test

--- src/test/java/com/galvanize/events/EventsControllerTests.java
+++ src/test/java/com/galvanize/events/EventsControllerTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -24,7 +25,8 @@ public class EventsControllerTests {
     EventsService eventsService;
     @Test
     public void initialGetNoEvents() throws Exception{
-       mockMvc.perform(MockMvcRequestBuilders.get("/api/event"))
+       when(eventsService.getEvents()).thenReturn(new EventList());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/event"))
                .andDo(print())
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty());
@@ -32,7 +34,7 @@ public class EventsControllerTests {
 
     @Test
     public void postEventReturnsEvent() throws Exception {
-        String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"startName\": \"Phil's Tiki Bar\",\"Address\": \"123 Example St\",\"City\": \"Normal\",\"State\": \"IL\",\"ZipCode\": 61761},\"endLocation\": {\"endName\": \"Greg's Oldtowne Tavern\",\"Address\": \"123 Example St\",\"City\": \"Normal\",\"State\": \"IL\",\"ZipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
+        String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"name\": \"Phil's Tiki Bar\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"endLocation\": {\"name\": \"Greg's Oldtowne Tavern\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
         mockMvc.perform(post("/api/event")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonEvent))

c2193ca43401c474b9549d85e61c766f7bf43be0
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-17 15:49:03 -0400

    GREEN: postEventReturnsEvent

--- src/test/java/com/galvanize/events/EventsControllerTests.java
+++ src/test/java/com/galvanize/events/EventsControllerTests.java
@@ -1,5 +1,6 @@
 package com.galvanize.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Date;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -23,6 +27,8 @@ public class EventsControllerTests {
 
     @MockBean
     EventsService eventsService;
+    ObjectMapper mapper = new ObjectMapper();
+
     @Test
     public void initialGetNoEvents() throws Exception{
        when(eventsService.getEvents()).thenReturn(new EventList());
@@ -34,15 +40,21 @@ public class EventsControllerTests {
 
     @Test
     public void postEventReturnsEvent() throws Exception {
-        String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"name\": \"Phil's Tiki Bar\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"endLocation\": {\"name\": \"Greg's Oldtowne Tavern\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
+        //String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"name\": \"Phil's Tiki Bar\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"endLocation\": {\"name\": \"Greg's Oldtowne Tavern\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
+        Address startAddress = new Address("Tiki Bar","555 Elm Street", "Anyplace", "GA", 55555);
+        Address endAddress = new Address("Tavern","555 Main Street", "Anyplace", "GA", 55555);
+        Date startDate= new Date(2001, 01, 01, 10,00, 00);
+        Date endDate= new Date(2001, 01, 02, 04,00, 00);
+        Event newEvent = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, "asdkfadsf", 50.01, 150.01, "planned", false);
+        when(eventsService.addEvent(any(Event.class))).thenReturn(newEvent);
         mockMvc.perform(post("/api/event")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonEvent))
+                        .content(mapper.writeValueAsString(newEvent)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.organization").value("Phils Buds"))
-                .andExpect(jsonPath("$.name").value("St. Patricks Bar Crawl '01"))
-                .andExpect(jsonPath("$.description").value("Phil's 21st Birthday Pub Crawl"));
+                .andExpect(jsonPath("$.name").value("St. Patricks Bar Crawl"))
+                .andExpect(jsonPath("$.description").value("21st Birthday Pub Crawl"));
         //todo validate other fields in response
     }
 

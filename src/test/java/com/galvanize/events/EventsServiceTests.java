8ebe657b1a028a1a38512d553ca2625c18ed5eec
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-17 16:14:54 -0400

    GREEN: EventsServiceTests addEven

--- src/test/java/com/galvanize/events/EventsServiceTests.java
+++ src/test/java/com/galvanize/events/EventsServiceTests.java
@@ -3,6 +3,7 @@ package com.galvanize.events;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -36,6 +37,22 @@ public class EventsServiceTests {
         EventList eventsList = eventsService.getEvents();
         assertThat(eventsList).isNotNull();
         assertThat(eventsList.isEmpty()).isFalse();
+    }
 
+    @Test
+    void addEvent() {
+        Address startAddress = new Address("Tiki Bar","555 Elm Street", "Anyplace", "GA", 55555);
+        Address endAddress = new Address("Tavern","555 Main Street", "Anyplace", "GA", 55555);
+        Date startDate= new Date(2001, 01, 01, 10,00, 00);
+        Date endDate= new Date(2001, 01, 02, 04,00, 00);
+        Event newEvent = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, "asdkfadsf", 50.01, 150.01, "planned", false);
+        when(eventsRepository.save(ArgumentMatchers.any(Event.class)))
+                .thenReturn(newEvent);
+        Event event = eventsService.addEvent(newEvent);
+        assertThat(event).isNotNull();
+        assertThat(event.getOrganization()).isEqualTo("Phils Buds");
+        assertThat(event.getName()).isEqualTo("St. Patricks Bar Crawl");
+        assertThat(event.getDescription()).isEqualTo("21st Birthday Pub Crawl");
+        //todo validate other fields
     }
 }

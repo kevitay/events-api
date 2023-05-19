8a4ce6dc91ff122ba6f73b79b5774073a2359f01
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-19 11:59:03 -0400

    RED: DeleteEvent Controller Test

--- src/test/java/com/galvanize/events/EventsServiceTests.java
+++ src/test/java/com/galvanize/events/EventsServiceTests.java
@@ -10,8 +10,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -75,5 +78,18 @@ public class EventsServiceTests {
         assertThat(event.getName()).isEqualTo("St. Patricks Bar Crawl");
         assertThat(event.getDescription()).isEqualTo("21st Birthday Pub Crawl");
         //todo validate other fields
+    }
+
+    @Test
+    void deleteEvent_byID() {
+        Event event = new Event("aabbcc1234", "Phils Buds", "St. Patricks Bar Crawl '01", "Social", "Phil's 21st Birthday Pub Crawl", "2001-01-01T16:00-04:00", "2001-01-02T02:00-04:00", "Phil's Tiki Bar", "123 Example St", "Normal", "IL", 61761, "Greg's Oldtowne Tavern", "123 Example St", "Normal", "IL",61761, "1", 50, 50, false;);
+        //String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"name\": \"Phil's Tiki Bar\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"endLocation\": {\"name\": \"Greg's Oldtowne Tavern\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
+        //event.setColor("RED");
+        when(eventsRepository.findById(UUID.fromString(anyString())))
+                .thenReturn(java.util.Optional.of(event));
+
+        eventsService.deleteEvent(event.getId());
+
+        verify(eventsRepository).delete(ArgumentMatchers.any(Event.class));
     }
 }

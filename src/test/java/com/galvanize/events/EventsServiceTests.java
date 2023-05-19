a8449048158ea1c22e4772e2294d59f08b45f41a
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-19 12:57:48 -0400

    GREEN: DeleteEventbyID Service Test

--- src/test/java/com/galvanize/events/EventsServiceTests.java
+++ src/test/java/com/galvanize/events/EventsServiceTests.java
@@ -80,13 +80,25 @@ public class EventsServiceTests {
         //todo validate other fields
     }
 
-    @Test
-    void deleteEvent_byID() {
-        Event event = new Event("aabbcc1234", "Phils Buds", "St. Patricks Bar Crawl '01", "Social", "Phil's 21st Birthday Pub Crawl", "2001-01-01T16:00-04:00", "2001-01-02T02:00-04:00", "Phil's Tiki Bar", "123 Example St", "Normal", "IL", 61761, "Greg's Oldtowne Tavern", "123 Example St", "Normal", "IL",61761, "1", 50, 50, false;);
-        //String jsonEvent = "{\"creatorId\": \"aabbcc1234\",\"organization\": \"Phils Buds\",\"name\": \"St. Patricks Bar Crawl '01\",\"type\": \"Social\",\"description\": \"Phil's 21st Birthday Pub Crawl\",\"startDateTime\": \"2001-01-01T16:00-04:00\",\"endDateTime\": \"2001-01-02T02:00-04:00\",\"startLocation\": {\"name\": \"Phil's Tiki Bar\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"endLocation\": {\"name\": \"Greg's Oldtowne Tavern\",\"address\": \"123 Example St\",\"city\": \"Normal\",\"state\": \"IL\",\"zipCode\": 61761},\"participantListId\": \"1\",\"base_cost\": \"50\",\"total_cost\": \"50\",\"status\": \"planned\",\"isPublic\": false}";
-        //event.setColor("RED");
-        when(eventsRepository.findById(UUID.fromString(anyString())))
-                .thenReturn(java.util.Optional.of(event));
+        @Test
+        void deleteEvent_byID() {
+            HashMap<String, String> startAddress = new HashMap<>();
+            startAddress.put("name", "Tiki Bar");
+            startAddress.put("address", "555 Elm Street");
+            startAddress.put("city", "Anyplace");
+            startAddress.put("state", "GA");
+            startAddress.put("zipcode", "55555");
+            HashMap<String, String> endAddress = new HashMap<>();
+            endAddress.put("name", "Tavern");
+            endAddress.put("address", "555 Main Street");
+            endAddress.put("city", "Anyplace");
+            endAddress.put("state", "GA");
+            endAddress.put("zipcode", "55555");
+            Date startDate= new Date(2001, 01, 01, 10,00, 00);
+            Date endDate= new Date(2001, 01, 02, 04,00, 00);
+            Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, "asdkfadsf", 50.01, 150.01, "planned", false);
+        when(eventsRepository.findById(ArgumentMatchers.any(UUID.class)))
+               .thenReturn(java.util.Optional.of(event));
 
         eventsService.deleteEvent(event.getId());
 

2c7549281bd92d760a25b57febe149b44ddcc4f9
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-25 11:30:12 -0400

    Refactor: UUID in  EventsService change UUID to Long datatype, EventsRepository remove import and change UUID to Long

--- src/main/java/com/galvanize/events/EventsRepository.java
+++ src/main/java/com/galvanize/events/EventsRepository.java
@@ -3,10 +3,10 @@ package com.galvanize.events;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+
 
 @Repository
-public interface EventsRepository extends JpaRepository<Event, UUID> {
+public interface EventsRepository extends JpaRepository<Event, Long> {
 
 
 }

b954bb78ddd175a6b86c4ce270b7a861df5a37e0
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-05-30 12:53:37 -0400

    Refactor: remove start End date and start end location from EventsServiceTests.java

--- src/test/java/com/galvanize/events/EventsServiceTests.java
+++ src/test/java/com/galvanize/events/EventsServiceTests.java
@@ -1,14 +1,11 @@
 package com.galvanize.events;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.*;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.*;
@@ -30,6 +27,7 @@ public class EventsServiceTests {
 
     @Test
     void getEvents() {
+        //todo mock call to itinerary api to get dates
         HashMap<String, String> startAddress = new HashMap<>();
         startAddress.put("name", "Tiki Bar");
         startAddress.put("address", "555 Elm Street");
@@ -44,7 +42,7 @@ public class EventsServiceTests {
         endAddress.put("zipcode", "55555");
         Date startDate= new Date(2001, 01, 01, 10,00, 00);
         Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findAll()).thenReturn(Arrays.asList(event));
         EventList eventsList = eventsService.getEvents();
         assertThat(eventsList).isNotNull();
@@ -53,6 +51,7 @@ public class EventsServiceTests {
 
     @Test
     public void getEventById() {
+        //todo mock call to itinerary api to get dates
         HashMap<String, String> startAddress = new HashMap<>();
         startAddress.put("name", "Tiki Bar");
         startAddress.put("address", "555 Elm Street");
@@ -67,7 +66,7 @@ public class EventsServiceTests {
         endAddress.put("zipcode", "55555");
         Date startDate= new Date(2001, 01, 01, 10,00, 00);
         Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(event));
 
@@ -88,21 +87,7 @@ public class EventsServiceTests {
 
     @Test
     void addEvent() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-        Event newEvent = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+        Event newEvent = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.save(ArgumentMatchers.any(Event.class)))
                 .thenReturn(newEvent);
         Event event = eventsService.addEvent(newEvent);
@@ -124,21 +109,7 @@ public class EventsServiceTests {
 
     @Test
     void updateEventReturnsEvent() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(event));
         when(eventsRepository.save(ArgumentMatchers.any(Event.class))).thenReturn(event);
@@ -158,21 +129,7 @@ public class EventsServiceTests {
 
     @Test
     public void updateEventStatusReturnsUpdatedEvent(){
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+        Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(event));
         when(eventsRepository.save(ArgumentMatchers.any(Event.class))).thenReturn(event);
@@ -195,21 +152,7 @@ public class EventsServiceTests {
 
     @Test
     public void updateEventStatusReturnsIllegalArgument() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+        Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findById(anyLong()))
                 .thenReturn(java.util.Optional.of(event));
         when(eventsRepository.save(ArgumentMatchers.any()))
@@ -220,213 +163,8 @@ public class EventsServiceTests {
     }
 
     @Test
-    public void updateEventDatesReturnsUpdatedEvent(){
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any(Event.class))).thenReturn(event);
-        Date newStartDate= new Date(2001, 01, 01, 6,00, 00);
-        Date newEndDate= new Date(2001, 01, 02, 10,00, 00);
-        Long id = event.getId();
-        Event updatedEvent = eventsService.updateEvent(10L, newStartDate, newEndDate);
-        assertThat(event).isNotNull();
-        assertThat(updatedEvent.getId()).isEqualTo(event.getId());
-        assertThat(updatedEvent.getStartDateTime()).isEqualTo(event.getStartDateTime());
-        assertThat(updatedEvent.getEndDateTime()).isEqualTo(event.getEndDateTime());
-    }
-
-    @Test
-    public void updateEventStartDateReturnsUpdatedEvent(){
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any(Event.class))).thenReturn(event);
-        Date newStartDate= new Date(2001, 01, 01, 6,00, 00);
-        Long id = event.getId();
-        Event updatedEvent = eventsService.updateEventStart(10L, newStartDate);
-        assertThat(event).isNotNull();
-        assertThat(updatedEvent.getId()).isEqualTo(event.getId());
-        assertThat(updatedEvent.getStartDateTime()).isEqualTo(event.getStartDateTime());
-    }
-
-    @Test
-    public void updateEventEndDateReturnsUpdatedEvent(){
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any(Event.class))).thenReturn(event);
-        Date newEndDate= new Date(2001, 01, 02, 10,00, 00);
-        Long id = event.getId();
-        Event updatedEvent = eventsService.updateEventEnd(10L, newEndDate);
-        assertThat(event).isNotNull();
-        assertThat(updatedEvent.getId()).isEqualTo(event.getId());
-        assertThat(updatedEvent.getEndDateTime()).isEqualTo(event.getEndDateTime());
-    }
-
-    @Test
-    public void updateEventDatesReturnIDNotExists() {
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        assertThatExceptionOfType(EventNotFoundException.class).isThrownBy(() -> {
-            eventsService.updateEvent( anyLong(), new Date(), new Date());
-        });
-    }
-
-    @Test
-    public void updateEventStartDateReturnIDNotExists() {
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        assertThatExceptionOfType(EventNotFoundException.class).isThrownBy(() -> {
-            eventsService.updateEventStart(anyLong(),new Date());
-        });
-    }
-
-    @Test
-    public void updateEventEndDateReturnIDNotExists() {
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        assertThatExceptionOfType(EventNotFoundException.class).isThrownBy(() -> {
-            eventsService.updateEventEnd(anyLong(),new Date());
-        });
-    }
-
-    @Test
-    public void updateEventDatesReturnsIllegalArgument() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any()))
-                .thenThrow(IllegalArgumentException.class);
-        assertThatExceptionOfType(InvalidEventUpdateException.class).isThrownBy(() ->{
-            eventsService.updateEvent(anyLong(), new Date(), new Date());
-        });
-    }
-
-    @Test
-    public void updateEventStartDateReturnsIllegalArgument() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any()))
-                .thenThrow(IllegalArgumentException.class);
-        assertThatExceptionOfType(InvalidEventUpdateException.class).isThrownBy(() ->{
-            eventsService.updateEventStart(anyLong(), new Date());
-        });
-    }
-
-    @Test
-    public void updateEventEndDateReturnsIllegalArgument() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
-        when(eventsRepository.findById(anyLong()))
-                .thenReturn(java.util.Optional.of(event));
-        when(eventsRepository.save(ArgumentMatchers.any()))
-                .thenThrow(IllegalArgumentException.class);
-        assertThatExceptionOfType(InvalidEventUpdateException.class).isThrownBy(() ->{
-            eventsService.updateEventEnd(anyLong(), new Date());
-        });
-    }
-
-    @Test
     void deleteEvent_byID() {
-        HashMap<String, String> startAddress = new HashMap<>();
-        startAddress.put("name", "Tiki Bar");
-        startAddress.put("address", "555 Elm Street");
-        startAddress.put("city", "Anyplace");
-        startAddress.put("state", "GA");
-        startAddress.put("zipcode", "55555");
-        HashMap<String, String> endAddress = new HashMap<>();
-        endAddress.put("name", "Tavern");
-        endAddress.put("address", "555 Main Street");
-        endAddress.put("city", "Anyplace");
-        endAddress.put("state", "GA");
-        endAddress.put("zipcode", "55555");
-        Date startDate= new Date(2001, 01, 01, 10,00, 00);
-        Date endDate= new Date(2001, 01, 02, 04,00, 00);
-         Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", startDate, endDate, startAddress, endAddress, 50.01, "planned", false);
+       Event event = new Event("AAADDD", "Phils Buds", "St. Patricks Bar Crawl", "Social", "21st Birthday Pub Crawl", 50.01, "planned", false);
         when(eventsRepository.findById(anyLong()))
                .thenReturn(java.util.Optional.of(event));
 

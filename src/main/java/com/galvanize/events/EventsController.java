74cc1099e1cc3a6c359f14a95059afaed7eb8736
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-06-01 15:53:53 -0400

    PreTest: code to call itinerary api

--- src/main/java/com/galvanize/events/EventsController.java
+++ src/main/java/com/galvanize/events/EventsController.java
@@ -4,6 +4,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,57 +20,67 @@ public class EventsController {
     }
 
     @GetMapping
-    public ExtEventList getEventList() {
+    public EventList getEventList() {
+        EventList eventList;
+        eventList = eventsService.getEvents();
+        return eventList;
+    }
+    @ResponseBody
+    @GetMapping("/extended")
+    public ExtEventList getExtEventList() {
         //first get list of event IDs
         EventList eventList;
         eventList = eventsService.getEvents();
-        List<Long> eventIds = eventList.getIDList();
+        List<Long> eventIdArray = eventList.getIDList();
 
         //then send that list to itinerary API to get dates and locations
-
-        String url = "http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/activities/getsummary";
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<List<Long>> httpEntity = new HttpEntity<>(eventIds, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<EventSummaryList> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, EventSummaryList.class);
-        //finally merge those with the original event details to return
         ExtEventList extEventList = new ExtEventList();
+        if(!eventList.isEmpty()) {
+            String url = "http://a08cb134e19c8438285f05f4a630b6bd-117037464.us-west-2.elb.amazonaws.com/api/activities/summaryList";
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+            HttpEntity<List<Long>> httpEntity = new HttpEntity<>(eventIdArray, headers);
+            RestTemplate restTemplate = new RestTemplate();
+            System.out.println(eventIdArray);
+            ResponseEntity<EventSummaryList> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, EventSummaryList.class);
+            //finally merge those with the original event details to return
+
 //      Iterate over each entry in the eventSummaryList returned from itinerary
-        for (int i =0; i < response.getBody().size(); i++) {
+            for (int i = 0; i < response.getBody().size(); i++) {
 //          Iterate over original event list to locate matching event for the current eventSummary
-            for (int j = 0; j < eventList.size(); j++) {
-                if(eventList.get(j).getId() == response.getBody().get(i).getEventId()) {
+                for (int j = 0; j < eventList.size(); j++) {
+                    if (eventList.get(j).getId() == response.getBody().get(i).getEventId()) {
 //                  Building starting and ending Hashmap to convert from Activity to desired format
-                    HashMap<String, String> startAddress = new HashMap<>();
-                    startAddress.put("address", response.getBody().get(i).getStartingActivity().getAddress());
-                    startAddress.put("city", response.getBody().get(i).getStartingActivity().getCity());
-                    startAddress.put("state", response.getBody().get(i).getStartingActivity().getState());
-                    startAddress.put("zipcode", response.getBody().get(i).getStartingActivity().getZipToString());
-                    HashMap<String, String> endAddress = new HashMap<>();
-                    endAddress.put("address", response.getBody().get(i).getEndingActivity().getAddress());
-                    endAddress.put("city", response.getBody().get(i).getEndingActivity().getCity());
-                    endAddress.put("state", response.getBody().get(i).getEndingActivity().getState());
-                    endAddress.put("zipcode", response.getBody().get(i).getEndingActivity().getZipToString());
+                        HashMap<String, String> startAddress = new HashMap<>();
+                        startAddress.put("address", response.getBody().get(i).getStartingActivity().getAddress());
+                        startAddress.put("city", response.getBody().get(i).getStartingActivity().getCity());
+                        startAddress.put("state", response.getBody().get(i).getStartingActivity().getState());
+                        startAddress.put("zipcode", response.getBody().get(i).getStartingActivity().getZipToString());
+                        HashMap<String, String> endAddress = new HashMap<>();
+                        endAddress.put("address", response.getBody().get(i).getEndingActivity().getAddress());
+                        endAddress.put("city", response.getBody().get(i).getEndingActivity().getCity());
+                        endAddress.put("state", response.getBody().get(i).getEndingActivity().getState());
+                        endAddress.put("zipcode", response.getBody().get(i).getEndingActivity().getZipToString());
 //                  Create instance of extEvent to start building our response body
-                    ExtEvent extEvent = new ExtEvent(eventList.get(j).getId(),
-                            eventList.get(j).getCreatorID(),
-                            eventList.get(j).getOrganization(),
-                            eventList.get(j).getName(),
-                            eventList.get(j).getType(),
-                            eventList.get(j).getDescription(),
-                            response.getBody().get(i).getStartingActivity().getStartTime(),
-                            response.getBody().get(i).getEndingActivity().getEndTime(),
-                            startAddress,
-                            endAddress,
-                            eventList.get(j).getBaseCost(),
-                            eventList.get(j).getStatus(),
-                            eventList.get(j).getPublic());
-                    extEventList.add(extEvent);
+                        ExtEvent extEvent = new ExtEvent(eventList.get(j).getId(),
+                                eventList.get(j).getCreatorID(),
+                                eventList.get(j).getOrganization(),
+                                eventList.get(j).getName(),
+                                eventList.get(j).getType(),
+                                eventList.get(j).getDescription(),
+                                response.getBody().get(i).getStartingActivity().getStartTime(),
+                                response.getBody().get(i).getEndingActivity().getEndTime(),
+                                startAddress,
+                                endAddress,
+                                eventList.get(j).getBaseCost(),
+                                eventList.get(j).getStatus(),
+                                eventList.get(j).getPublic());
+                        extEventList.add(extEvent);
+                    }
                 }
             }
         }
-        return extEventList;
+    return extEventList;
     }
 
     @GetMapping("/{id}")
